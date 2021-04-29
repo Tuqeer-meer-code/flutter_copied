c9930bc077796c1ff0a25f5fed8b3b995c6dc847
Tuqeer khan <82929102+Tuqeer-meer-code@users.noreply.github.com>
2021-04-29 22:04:04 +0500

    initiall Commit

---
+++ lib/screens/pallete.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/painting.dart';
+
+class plte {
+  static const Color iconColor = Color(0xFF0B5526);
+  static const Color btnColor = Color(0xFF519B6C);
+  static const Color backgroundColor = Color(0xFF82CC9D);
+  static const Color textColor = Color(0xFFC7F8DF);
+  static const Color activateColor = Color(0xFF82CCA0);
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'pallete.dart';
+class signupScreen extends StatefulWidget {
+  @override
+  _signupScreenState createState() => _signupScreenState();
+}
+
+class _signupScreenState extends State<signupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: plte.backgroundColor,
+      body: Stack(
+        children: [
+          Positioned(
+              top: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                height: 300,
+
+                child: Container(
+                  padding: EdgeInsets.only(top: 90, left: 20),
+                  color: Color(0xFF3b5999).withOpacity(.30),
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                            text: "Welcome to" ,
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: plte.textColor,
+                                letterSpacing: 1),
+                            children: [
+                              TextSpan(
+                                  text: " US & CB ",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      color: plte.activateColor,
+                                      fontWeight: FontWeight.bold)),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "SignUp to continue",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              )),
+
+          Positioned(
+              top: 200,
+              child: Container(
+                  height: 250,
+                  width: MediaQuery.of(context).size.width - 40,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: plte.backgroundColor,
+                      borderRadius: BorderRadius.circular(15),
+
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 15,
+                            spreadRadius: 5)
+                      ]),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextField(
+
+
+
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.eighteen_mp
+                                ,color: plte.iconColor,),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0))
+
+                              )
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+
+
+              )
+          ) ],
+      ),
+    );
+  }
+}
