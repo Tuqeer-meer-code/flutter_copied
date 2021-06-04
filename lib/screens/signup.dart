348cfe9804b24d861fb76f84822db86890ca033c
Tuqeer khan <82929102+Tuqeer-meer-code@users.noreply.github.com>
2021-06-04 23:57:37 +0500

    added notication color

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:uscb/screens/mainscreen.dart';
 import 'login.dart';
 import 'pallete.dart';
+
 class signupScreen extends StatefulWidget {
   @override
   _signupScreenState createState() => _signupScreenState();
@@ -10,176 +12,214 @@ class signupScreen extends StatefulWidget {
 class _signupScreenState extends State<signupScreen> {
   @override
   int group = 1;
-  var _formkey=GlobalKey<FormState>();
+  var _formkey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: plte.backgroundColor,
-      body: Stack(
-        children: [
-          Positioned(
-              top: 0,
-              right: 0,
-              left: 0,
-              child: Container(
-                height: 200,
-                child: Container(
-                  padding: EdgeInsets.only(top: 50, left: 20),
-                  color: Color(0xFF3b5999).withOpacity(.30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                            text: "Welcome to",
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: plte.btnColor
+    ));
+    return
+      Scaffold(
+          backgroundColor: plte.backgroundColor,
+          body: Stack(
+            children: [
+              Positioned(
+                  top: 0,
+                  right: 0,
+                  left: 0,
+                  child: Container(
+                    height: 200,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 50, left: 20),
+                      color: Color(0xFF3b5999).withOpacity(.30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                                text: "Welcome to",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: plte.textColor,
+                                    letterSpacing: 1),
+                                children: [
+                                  TextSpan(
+                                      text: " US & CB ",
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          color: plte.activateColor,
+                                          fontWeight: FontWeight.bold)),
+                                ]),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "SignUp to continue",
                             style: TextStyle(
-                                fontSize: 18,
-                                color: plte.textColor,
-                                letterSpacing: 1),
-                            children: [
-                              TextSpan(
-                                  text: " US & CB ",
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      color: plte.activateColor,
-                                      fontWeight: FontWeight.bold)),
-                            ]),
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
                       ),
+                    ),
+                  )),
+              Positioned(
+                  top: 120,
+                  child: Container(
+                      padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                      height: 400,
+                      width: MediaQuery.of(context).size.width - 40,
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(
+                          color: plte.backgroundColor,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 10,
+                                spreadRadius: 5)
+                          ]),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: 15.0,
+                          right: 15.0,
+                        ),
+                        child: Form(
+                          key: _formkey,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                textfield("Enter Name"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter Cnic"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter Email"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter House address"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter Province"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter District"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter Tehsil"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Enter City"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                textfield("Password"),
+                                SizedBox(
+                                  height: 150,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ))),
+              Positioned(
+                top: 530,
+                left: 30,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
                       SizedBox(
-                        height: 5,
+                        height: 10,
                       ),
-                      Text(
-                        "SignUp to continue",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )
+                      MaterialButton(
+                          hoverColor: plte.backgroundColor,
+                          elevation: 3,
+                          minWidth: 100,
+                          splashColor: plte.textColor,
+                          shape: StadiumBorder(),
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(
+                                color: plte.textColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          color: plte.btnColor,
+                          onPressed: () {
+                            setState(() {
+                              if (_formkey.currentState.validate()) {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return mainScreen();
+                                }));
+                              }
+                            });
+                          }),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 50),
+                            child: Text(
+                              "Already have an account?",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          TextButton(
+                            child: Text("Log In"),
+                            onPressed: () {
+                              setState(() {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return loginScreen();
+                                }));
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
-              )),
-          Positioned(
-              top: 120,
-              child: Container(
-                padding: EdgeInsets.only(top: 10,left: 10,right: 10),
-                  height: 380,
-                  width: MediaQuery.of(context).size.width - 40,
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                      color: plte.backgroundColor,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            blurRadius: 10,
-                            spreadRadius: 5)
-                      ]),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 15.0,right: 15.0, ),
-
-                    child: Form(
-
-                      key: _formkey,
-                      child: SingleChildScrollView(
-                        child: Column(
-                           crossAxisAlignment: CrossAxisAlignment.center,
-                           mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            textfield("Enter Name"),
-                            SizedBox(
-                              height: 10,
-                            ),
-                        textfield("Enter Cnic")
-                        ,   SizedBox(height: 10,),
-                        textfield("Enter Email")
-                        ,   SizedBox(height: 10,),
-                            textfield("Enter House address")
-                            ,   SizedBox(height: 10,),
-                            textfield("Enter Province")
-                        ,   SizedBox(height: 10,),
-                            textfield("Enter District")
-                        ,   SizedBox(height: 10,),
-                            textfield("Enter Tehsil")
-                        ,   SizedBox(height: 10,),
-                            textfield("Enter City")
-                        ,   SizedBox(height: 10,),
-                          textfield("Password"),
-                            SizedBox(height: 10,),
-                            MaterialButton(
-                              elevation: 5,
-                                minWidth: 100,
-                              splashColor: plte.textColor,
-                              shape: StadiumBorder(),
-                              child: Text("Sign up",style: TextStyle(color: plte.textColor,fontSize: 16,fontWeight: FontWeight.bold),),
-                              color: plte.btnColor,
-
-                                onPressed: (){
-
-                                setState(() {
-                                  if(_formkey.currentState.validate()) {
-                                    Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) {
-                                          return mainScreen();
-                                        }));
-                                  }
-                                });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
               )
+            ],
           ),
-          Positioned(
-              top: 500,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Text("Already have an account?",style: TextStyle(color: Colors.white),),
-                  ),
-
-                   TextButton(
-
-                  child: Text("Log In"),
-                    onPressed: (){
-                    setState(() {
-                    Navigator.push(context, MaterialPageRoute(builder: (context){
-                      return loginScreen();
-                    }));
-                    });
-                    },
-                  ),
-                ],
-              ),
-            )
-        ],
-      ),
-    );
+        );
   }
 }
-Widget textfield(String title){
-  return
-  TextFormField(
-    validator: (String value){
-      if(value.isEmpty){
+
+Widget textfield(String title) {
+  return TextFormField(
+    validator: (String value) {
+      if (value.isEmpty) {
         return "Required";
       }
     },
     decoration: InputDecoration(
-errorStyle: TextStyle(letterSpacing: 1,color: Colors.white,fontSize: 14,),
-
+      errorStyle: TextStyle(
+        letterSpacing: 1,
+        color: Colors.white,
+        fontSize: 14,
+      ),
       prefixIcon: Icon(
         Icons.account_circle_outlined,
         color: plte.iconColor,
       ),
       border: OutlineInputBorder(
-        borderRadius:
-        BorderRadius.all(Radius.circular(30.0)),
+        borderRadius: BorderRadius.all(Radius.circular(30.0)),
       ),
       contentPadding: EdgeInsets.all(10.0),
       hintText: title,
-
     ),
   );
-
 }
